1e1ee2a8c930013ad15b90e42e150ab9ca848816
Rohit <rohit.bias@gmail.com>
2019-04-08 22:26:19 -0400

    Updated Fibonacii

--- src/recursionPrograms/Fibonacii.java
+++ src/recursionPrograms/Fibonacii.java
@@ -4,7 +4,22 @@ public class Fibonacii {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int index=0;
+		while(true)
+		{
+			System.out.println(fibonacii(index));
+			index++;
+		}
+		
 
+	}
+
+	public static long fibonacii(int i) {
+		if (i==0)return 0;
+		if (i<=2) return 1;
+		long fibonac= fibonacii(i-1)+fibonacii(i-2);
+		// TODO Auto-generated method stub
+		return fibonac;
 	}
 
 }

99ae40d4704f516642ab2a4c9c676c694a7c71a7
Rohit <rohit.bias@gmail.com>
2020-03-11 20:01:15 -0400

    Program Completed.

--- src/datatypes/DataTypes.java
+++ src/datatypes/DataTypes.java
@@ -1,7 +1,5 @@
 package datatypes;
 
-import java.util.Scanner;
-
 /*
 boolean 1 byte
 char 	2 byte
@@ -16,40 +14,16 @@ double  8 byte
 public class DataTypes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 
+		boolean flag= true;
+		char ch='c';
+		byte b='a';
+		short sh='a';
+		int n=8;
+		float fl=4.6f;
+		long l=10000;
+		double d= 9898;
 
 		
-		Scanner in = new Scanner(System.in);
-
-        int n = in.nextInt();
-        String arr[] = new String[n];
-
-        in.nextLine();
-
-        for (int i = 0; i < n; i++){
-            arr[i] = in.nextLine();
-        }
-
-        for (int i = 0; i < n; i++){
-            try{
-                long l = Long.parseLong(arr[i]);
-                System.out.println(arr[i] + " can be fitted in:");
-
-                if(l <= 127 && l >= -128){
-                    System.out.println("* byte \n* short \n* int \n* long");
-                }else if( l <= 32767 && l >= -32768){
-                    System.out.println("* short \n* int \n* long");
-                }else if ( l <= 2147483647 && l >= -2147483648){
-                    System.out.println("* int \n* long");
-                }else if(l <= 9223372036854775807l && l >= -9223372036854775808l){
-                    System.out.println("* long");
-                }
-
-            }catch (NumberFormatException e){
-                System.out.println(arr[i] + " can't be fitted anywhere.");
-            }
-        }
-
-
     }
 }

0ad61e3b9b5852aab924541e5eb61ef834a1001a
Rohit Bhatt (Contractor) <Rohit-Bhatt@na.odcorp.net>
2020-03-12 14:15:57 -0400

    HashSetExample Completed

--- src/collectionframework/HashSetExample.java
+++ src/collectionframework/HashSetExample.java
@@ -1,10 +1,27 @@
 package collectionframework;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class HashSetExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		HashSet<String> set = new HashSet<String>();
+		set.add("a");
+		set.add("e");
+		set.add("i");
+		set.add("o");
+		set.add("u");
+		set.add("a");
+		set.add("e");
+		System.out.println(set);
 
+		Iterator<String> itr = set.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+	}
 }

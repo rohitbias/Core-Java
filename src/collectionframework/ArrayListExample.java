97f23b36fa9b392911758a11f454961454a8bc04
Rohit <rohit.bias@gmail.com>
2020-03-11 20:34:52 -0400

    Update ArrayListExample.java
    Program Completed.

--- src/collectionframework/ArrayListExample.java
+++ src/collectionframework/ArrayListExample.java
@@ -1,9 +1,30 @@
 package collectionframework;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class ArrayListExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ArrayList<String> list= new ArrayList<String>();
+		list.add("a");
+		list.add("e");
+		list.add("i");
+		list.add("o");
+		list.add("u");
+		list.add("b");
+		list.add("c");
+		System.out.println(list);
+		
+		Iterator<String> itr=list.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+		
+		
 
 	}
 

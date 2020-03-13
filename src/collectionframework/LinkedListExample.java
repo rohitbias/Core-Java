35f65b155e60a126bc9f27b04501137218d086a8
Rohit Bhatt (Contractor) <Rohit-Bhatt@na.odcorp.net>
2020-03-12 20:15:02 -0400

    LinkedListExample Completed

--- src/collectionframework/LinkedListExample.java
+++ src/collectionframework/LinkedListExample.java
@@ -1,9 +1,26 @@
 package collectionframework;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class LinkedListExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		LinkedList<String> list = new LinkedList<String>();
+		list.add("a");
+		list.add("e");
+		list.add("i");
+		list.add("o");
+		list.add("u");
+		list.add("b");
+		list.add("c");
+		System.out.println(list);
+
+		ListIterator<String> itr = list.listIterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
 
 	}
 
